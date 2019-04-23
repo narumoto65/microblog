e604b8103766d1f7ace9b9997f7e6b8b8041b9a2
narumoto65 <narumoto65@gmail.com>
2019-04-24 03:49:55 +0900

    django practice

---
+++ blog/views.py
@@ -0,0 +1,58 @@
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
+from .models import Blog
+from .forms import BlogForm
+
+
+class BlogListView(ListView):
+    model = Blog
+    context_object_name = "blogs"
+    paginate_by = 5
+class BlogDetailView(DetailView):
+    model = Blog
+    context_object_name = "blog"
+class BlogCreateView(LoginRequiredMixin,CreateView):
+    model = Blog
+    form_class = BlogForm
+    #fields = ["content"]
+    success_url = reverse_lazy("index")
+    template_name="blog/blog_create_form.html"
+
+    login_url='/login'
+    def form_valid(self,form):
+        messages.success(self.request,"保存しますた")
+        return super().form_valid(form)
+    
+    def form_invalid(self,form):
+        messages.error(self.request,"保存に失敗したぽ")
+        return super().form_invalid(form)
+
+class BlogUpdateView(LoginRequiredMixin,UpdateView):
+    model= Blog
+    form_class=BlogForm #form.htmlで用意したmodelformを使用
+    #fields = ["content",]
+    template_name="blog/blog_update_form.html"
+    login_url = "/login"
+    def get_success_url(self):
+        blog_pk = self.kwargs['pk']
+        url = reverse_lazy("detail",kwargs={"pk":blog_pk})
+        return url
+    def form_valid(self,form):
+        messages.success(self.request,"更新されました")
+        return super().form_valid(form)
+    def form_invalid(self,form):
+        messages.error(self.request,"更新できませんでした")
+        return super().form_invalid(form)
+class BlogDeleteView(LoginRequiredMixin,DeleteView):
+    model=Blog
+    success_url = reverse_lazy("index")
+    login_url="/login"
+    def delete(self,request,*args,**kwargs):
+        messages.success(self.request,"削除しました")
+        return super().delete(request,*args,**kwargs)
